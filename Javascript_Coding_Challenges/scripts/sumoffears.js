85f22aa2c283521fd0823e514bf3f74ce785aa74
Josh Casteel <43053467+joshcasteel@users.noreply.github.com>
2020-04-12 22:50:45 -0400

    fixed sum of fears int issue

--- Javascript_Coding_Challenges/scripts/sumoffears.js
+++ Javascript_Coding_Challenges/scripts/sumoffears.js
@@ -1,31 +1,34 @@
 ﻿const searchFears = () => {
-    let input = document.getElementById("fearNum").value;
-    /*let arr1 = document.getElementById("arr1").value;
-    let arr1 = document.getElementById("arr2").value;
-    let arr1 = document.getElementById("arr3").value;
-    let arr1 = document.getElementById("arr4").value;
-    let arr1 = document.getElementById("arr5").value;
+    let input = parseInt(document.getElementById("fearNum").value);
+    let num1 = parseInt(document.getElementById("num1").value);
+    let num2 = parseInt(document.getElementById("num2").value);
+    let num3 = parseInt(document.getElementById("num3").value);
+    let num4 = parseInt(document.getElementById("num4").value);
+    let num5 = parseInt(document.getElementById("num5").value);
 
-    let ArrFear = [arr1, arr2, arr3, arr4, arr5];*/
+    let ArrNums = [num1, num2, num3, num4, num5];
     
-    let ArrPairs = [];
+  
     //removes numbers from array that are larger than input
-    let possibleNums = ArrFear.filter(x => x < input);
+    let filterNums = ArrNums.filter(x => x < input);
+    console.log(filterNums)
+
+    //array to store matches
+    let ArrPairs = [];
 
     //nested loops checks each number of the array
-    for (let i = 0; i < possibleNums.length; i++) {
-        for (let k = 0; k < possibleNums.length; k++) {
-            if (possibleNums[i] + possibleNums[k] == input) {
-                ArrPairs.push([possibleNums[i], possibleNums[k]]);
+    for (let i = 0; i < filterNums.length; i++) {
+        for (let k = 0; k < filterNums.length; k++) {
+            if (filterNums[i] + filterNums[k] == input) {
+                ArrPairs.push([filterNums[i], filterNums[k]]);
 
-            } 
+            }
         }
     }
-    
-    
+
     if (ArrPairs.length == 0) {
         document.getElementById("fearResults").innerHTML = "No matches found";
-    } else {   
+    } else {
 
         //nested loop results in doubles like 7+3 and 3+7. Halving it removes the doubles
         let ArrLeng = ArrPairs.length / 2;
@@ -37,9 +40,15 @@
         });
         document.getElementById("fearResults").innerHTML = resultString;
     }
+    
 }
 
 const clearFears = () => {
     document.getElementById("fearResults").innerHTML = "";
     document.getElementById("fearNum").value = "";
+    document.getElementById("num1").value = 10;
+    document.getElementById("num2").value = 15;
+    document.getElementById("num3").value = 3;
+    document.getElementById("num4").value = 7;
+    document.getElementById("num5").value = 2;
 }
